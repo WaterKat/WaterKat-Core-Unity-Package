7e6cd35795876339d9d6fd33cc666b47a9a25331
WaterKat_ROG <WaterKatGames@gmail.com>
2021-07-11 11:51:37 -0700

    Adding Script Templates

---
+++ Editor/WaterkatCoreEditorWindow.cs
@@ -0,0 +1,40 @@
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace WaterKat.Core
+{
+    public class WaterkatCoreEditorWindow : EditorWindow
+    {
+        private void OnGUI()
+        {
+            EditorGUILayout.LabelField("WaterKat Script Templates",EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("These are some basic script templates for faster prototyping");
+            EditorGUILayout.LabelField("Unity may require you to restard Unity in order to load these templates");
+            if (GUILayout.Button("Copy Waterkat Script Templates"))
+            {
+                string templatePath = "Packages/com.waterkat.core/ScriptTemplates";
+                string destinationPath = "Assets/ScriptTemplates";
+
+                if (!AssetDatabase.IsValidFolder(destinationPath)) { AssetDatabase.CreateFolder("Assets", "ScriptTemplates"); }
+
+                string[] assetGUIDS = AssetDatabase.FindAssets("", new string[] { templatePath });
+                foreach (var assetGUID in assetGUIDS)
+                {
+                    string stringPath = AssetDatabase.GUIDToAssetPath(assetGUID);
+                    AssetDatabase.CopyAsset(stringPath, destinationPath + stringPath.Substring(stringPath.LastIndexOf('/')));
+                }
+
+                AssetDatabase.Refresh();
+            }/*
+            position = 
+                .width = ;*/
+        }
+
+        [MenuItem("Window/WaterKat/Core Editor Window")]
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(WaterkatCoreEditorWindow), true, "Core Editor", true);
+        }
+    }
+}
